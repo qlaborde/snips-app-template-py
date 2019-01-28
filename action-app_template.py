ba5ef61febecc52e34de3fdfbfe71d83c0d5e158
qlaborde <qlaborde@evertygo.com>
2019-01-28 09:59:46 +0100

    test to add about skill

--- action-app_template.py
+++ action-app_template.py
@@ -1,3 +1,9 @@
+# @Author: laborde
+# @Date:   2019-01-28T09:51:58+01:00
+# @Email:  qlaborde@evertygo.com
+# @Last modified by:   laborde
+# @Last modified time: 2019-01-28T09:56:23+01:00
+
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
@@ -5,6 +11,7 @@ from snipsTools import SnipsConfigParser
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
+import requests
 
 CONFIG_INI = "config.ini"
 
@@ -15,11 +22,7 @@ MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
-class Template(object):
-    """Class used to wrap action code with mqtt connection
-        
-        Please change the name refering to your application
-    """
+class ImperiHome(object):
 
     def __init__(self):
         # get the configuration if needed
@@ -30,19 +33,9 @@ class Template(object):
 
         # start listening to MQTT
         self.start_blocking()
-        
+
     # --> Sub callback function, one per intent
-    def intent_1_callback(self, hermes, intent_message):
-        # terminate the session first if not continue
-        hermes.publish_end_session(intent_message.session_id, "")
-        
-        # action code goes here...
-        print '[Received] intent: {}'.format(intent_message.intent.intent_name)
-
-        # if need to speak the execution result by tts
-        hermes.publish_start_session_notification(intent_message.site_id, "Action1 has been done", "")
-
-    def intent_2_callback(self, hermes, intent_message):
+    def about_callback(self, hermes, intent_message):
         # terminate the session first if not continue
         hermes.publish_end_session(intent_message.session_id, "")
 
@@ -50,20 +43,14 @@ class Template(object):
         print '[Received] intent: {}'.format(intent_message.intent.intent_name)
 
         # if need to speak the execution result by tts
-        hermes.publish_start_session_notification(intent_message.site_id, "Action2 has been done", "")
-
-    # More callback function goes here...
+        hermes.publish_start_session_notification(intent_message.site_id, "About has been done", "")
 
     # --> Master callback function, triggered everytime an intent is recognized
     def master_intent_callback(self,hermes, intent_message):
         coming_intent = intent_message.intent.intent_name
-        if coming_intent == 'intent_1':
-            self.intent_1_callback(hermes, intent_message)
-        if coming_intent == 'intent_2':
-            self.intent_2_callback(hermes, intent_message)
-
-        # more callback and if condition goes here...
-
+        if coming_intent == 'evertygo:about':
+            self.about_callback(hermes, intent_message)
+        
     # --> Register callback function and start MQTT
     def start_blocking(self):
         with Hermes(MQTT_ADDR) as h:
